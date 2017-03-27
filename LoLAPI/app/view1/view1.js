360fb9a311cecfd51baa8374d18fa2d74040a606
MaximeLD <maxime.leduc.09@gmail.com>
2017-03-27 15:16:22 +0200

    LoLAPI - Added APIKEY

--- LoLAPI/app/view1/view1.js
+++ LoLAPI/app/view1/view1.js
@@ -2,6 +2,8 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
+var LoL_API_KEY = '?api_key=RGAPI-DEB9D447-AA77-4FF5-858A-FB4DBD8B3D88'
+
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
